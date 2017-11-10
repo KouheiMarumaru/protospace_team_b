fa7cdef7c4c965e34d8244d12210a660fbc65631
matumaru <matumarukouhei@gmail.com>
2017-11-10 09:35:22 +0900

    edit削除

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,14 +22,9 @@ class PrototypesController < ApplicationController
   def show
   end
 
-
   def destroy
     @prototype.destroy
     redirect_to :root, notice: 'Prototype was successfully deleted.'
-  end
-
-  def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   private
