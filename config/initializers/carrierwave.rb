4d5848c4ce54a7cabcb12f565d5c2575b59f07c0
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-18 11:25:46 +0900

    エラー修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: ''
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'protospaceteamb'
-  config.asset_host = 'https://s3-ap-southeast-2.amazonaws.com/protospaceteamb'
+  config.asset_host = 'http://s3-ap-northeast-1.amazonaws.com'
 end
