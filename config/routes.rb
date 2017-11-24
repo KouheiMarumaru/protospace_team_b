b1f79c98a68881fded80cfe9444c5a6f89e9435a
masanori123 <motomuramasanori@MasasMacBook-Pro.local>
2017-11-24 09:54:37 +0900

    コメント編集機能の実装
    ビューを作成
    リンク先の指定
    コントローラの編集
    ルーティングの指定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :prototypes do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :edit, :update]
   end
   resources :users, only: [:show, :edit, :update]
 end
