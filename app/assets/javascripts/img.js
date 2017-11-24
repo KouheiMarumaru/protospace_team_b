668a4fdb55e9c65d99b0231a23da11e44340dfb8
matumaru <matumarukouhei@gmail.com>
2017-11-24 11:51:46 +0900

    画像関連

--- app/assets/javascripts/img.js
+++ app/assets/javascripts/img.js
@@ -1,6 +1,6 @@
 $(function(){
   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
-  $('form').on('change', 'input[type="file"]', function(e) {
+  $('form').on('change', '.main_file_field', function(e) {
     var file = e.target.files[0],
         reader = new FileReader(),
         $preview = $(".cover-image-upload");
@@ -22,6 +22,108 @@ $(function(){
                   src: e.target.result,
                   width: "150px",
                   id: "main_preview",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
+
+$(function(){
+  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
+  $('form').on('change', '.sub_file_field_0', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".image-upload_0");
+        t = this;
+
+    // 画像ファイル以外の場合は何もしない
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    // ファイル読み込みが完了した際のイベント登録
+    reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        // $preview.empty();
+        $("#sub_preview_0").remove();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  width: "100%",
+                  height: "100%",
+                  id: "sub_preview_0",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
+
+$(function(){
+  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
+  $('form').on('change', '.sub_file_field_1', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".image-upload_1");
+        t = this;
+
+    // 画像ファイル以外の場合は何もしない
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    // ファイル読み込みが完了した際のイベント登録
+    reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        // $preview.empty();
+        $("#sub_preview_1").remove();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  width: "100%",
+                  height: "100%",
+                  id: "sub_preview_1",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
+
+$(function(){
+  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
+  $('form').on('change', '.sub_file_field_2', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".image-upload_2");
+        t = this;
+
+    // 画像ファイル以外の場合は何もしない
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    // ファイル読み込みが完了した際のイベント登録
+    reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        // $preview.empty();
+        $("#sub_preview_2").remove();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  width: "100%",
+                  height: "100%",
+                  id: "sub_preview_2",
                   title: file.name
               }));
       };
